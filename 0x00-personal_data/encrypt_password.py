7bbc25656e41bd6d807a9397561f4e708a5d887f
Emmanuel Nwafor <nwaforemmanuel005@gmail.com>
2023-11-04 15:33:49 +0100

    docs

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -7,8 +7,10 @@ import bcrypt
 
 
 def hash_password(password: str) -> ByteString:
+    """encrypt password"""
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
+    """check encrypted password"""
     return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
