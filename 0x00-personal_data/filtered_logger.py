17968bfc4c7b66c20be58e672f19e9a86258b7f1
Emmanuel Nwafor <nwaforemmanuel005@gmail.com>
2023-11-04 05:12:42 -0700

    rm

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -10,7 +10,6 @@ def filter_datum(fields, redaction, message, separator):
     filter datum
     """
     for field in fields:
-        print(message)
         pattern = fr'({re.escape(field)})=.+?{separator}'
         repl = fr'\1={redaction}{separator}'
         message = re.sub(pattern, repl, message)
