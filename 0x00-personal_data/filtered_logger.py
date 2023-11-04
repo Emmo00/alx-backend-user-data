bbbf0247b2527dffc2f17abc8523764f7c604ebc
Emmanuel Nwafor <nwaforemmanuel005@gmail.com>
2023-11-04 13:32:34 +0100

    dic

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -8,6 +8,7 @@ from typing import List
 
 def filter_datum(fields: List[str], redaction: str, message: str,
                  separator: str) -> str:
+    """filter datum"""
     for field in fields:
         message = re.sub(r"(?<={}=)[^{}]*(?={})".format(field,
                                                         separator, separator),
