fd046f63ee5f7732591779f9ee08482cbabaef49
Emmanuel Nwafor <nwaforemmanuel005@gmail.com>
2023-11-04 13:53:50 +0100

    p

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -16,11 +16,14 @@ class RedactingFormatter(logging.Formatter):
     SEPARATOR = ";"
 
     def __init__(self, fields: Iterable[str]):
+        """initialize formatter"""
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
-        return (filter_datum(self.fields, self.REDACTION, super().format(record), self.SEPARATOR))
+        """format message log"""
+        return filter_datum(self.fields, self.REDACTION,
+                            super().format(record), self.SEPARATOR)
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,
