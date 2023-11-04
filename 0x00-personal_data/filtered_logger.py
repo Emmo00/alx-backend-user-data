a0161fef852c9760bbf84cf5bc112c6ed5d1f739
Emmanuel Nwafor <nwaforemmanuel005@gmail.com>
2023-11-04 05:21:03 -0700

    anno

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,12 +3,10 @@
 filter logger module
 """
 import re
+from typing import List
 
 
-def filter_datum(fields, redaction, message, separator):
-    """
-    filter datum
-    """
+def filter_datum(fields: List, redaction: str, message: str, separator: str) -> str:
     for field in fields:
         pattern = fr'({re.escape(field)})=.+?{separator}'
         repl = fr'\1={redaction}{separator}'
