98260a8ed2e9b4ad5c52276746afcb6bf92ada0a
Emmanuel Nwafor <nwaforemmanuel005@gmail.com>
2023-11-08 09:04:12 +0100

    extract

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -31,3 +31,15 @@ class BasicAuth(Auth):
                 decode('ascii')
         except Exception:
             return None
+
+    def extract_user_credentials(
+            self, decoded_base64_authorization_header: str) -> (str, str):
+        """extract user credentials from decoded header
+        """
+        if decoded_base64_authorization_header is None:
+            return None, None
+        if type(decoded_base64_authorization_header) is not str:
+            return None, None
+        if ':' not in decoded_base64_authorization_header:
+            return None, None
+        return tuple(decoded_base64_authorization_header.split(':'))
