8109027204cffa377fc7dac7e51dafa521c47384
Emmanuel Nwafor <nwaforemmanuel005@gmail.com>
2023-11-08 07:52:43 +0100

    require auth"

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -11,7 +11,13 @@ class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """require authentication
         """
-        return False
+        if path is None:
+            return True
+        path += '/' if path[-1] != '/' else ''
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        if path in excluded_paths:
+            return False
 
     def authorization_header(self, request=None) -> str:
         """authorization header
