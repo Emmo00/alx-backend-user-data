3062e5100ea9826a25e79aa598de894c2a2f80aa
Emmanuel Nwafor <nwaforemmanuel005@gmail.com>
2023-11-09 15:22:07 +0100

    neg

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -22,9 +22,10 @@ def login():
     if not result or len(result) == 0:
         return jsonify({'error': 'no user found for this email'}), 404
     user = result[0]
-    if user.is_valid_password(password):
+    if not user.is_valid_password(password):
         return jsonify({'error': 'wrong password'}), 401
     from api.v1.app import auth
     session_id = auth.create_session(user.id)
     response = jsonify(user.to_json())
     response.set_cookie(getenv('SESSION_NAME'), session_id)
+    return response
