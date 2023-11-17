f742207f0a54371484cfe570553d9236201f131d
Emmanuel Nwafor <nwaforemmanuel005@gmail.com>
2023-11-17 03:19:20 +0100

    3_19

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """auth module
 """
+import uuid
 import bcrypt
 from db import DB, NoResultFound
 
@@ -11,6 +12,12 @@ def _hash_password(password: str):
     """return hashed password
     """
     return bcrypt.hashpw(password.encode(), salt)
+
+
+def _generate_uuid():
+    """return new uuid string
+    """
+    return str(uuid.uuid4())
 
 
 class Auth:
@@ -31,3 +38,12 @@ class Auth:
             user = self._db.add_user(email, _hash_password(password))
             return user
         raise ValueError(f"User {email} already exists")
+
+    def valid_login(self, email: str, password: str):
+        """validate login
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            return bcrypt.checkpw(password.encode(), user.hashed_password)
+        except Exception:
+            return False
