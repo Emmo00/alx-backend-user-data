0df47a47c78adb70feb0e798da7e42e0c17a2e51
Emmanuel Nwafor <nwaforemmanuel005@gmail.com>
2023-11-17 03:12:29 +0100

    register

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 """simple flask app
 """
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route('/')
@@ -11,6 +13,19 @@ def root():
     """root route
     """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def register_user():
+    """register user
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+    try:
+        AUTH.register_user(email, password)
+        return jsonify({'email': email, 'message': 'user created'})
+    except ValueError:
+        return jsonify({'message': 'email already registered'}), 400
 
 
 if __name__ == '__main__':
