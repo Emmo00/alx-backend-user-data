293bb48bf8f83bd26ac67f029c245c03ab435c0d
Emmanuel Nwafor <nwaforemmanuel005@gmail.com>
2023-11-17 02:58:35 +0100

    auth

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -2,6 +2,7 @@
 """auth module
 """
 import bcrypt
+from db import DB, NoResultFound
 
 salt = bcrypt.gensalt()
 
@@ -10,3 +11,23 @@ def _hash_password(password: str):
     """return hashed password
     """
     return bcrypt.hashpw(password.encode(), salt)
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        """initialize
+        """
+        self._db = DB()
+
+    def register_user(self, email: str, password: str):
+        """register user
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            user = self._db.add_user(email, _hash_password(password))
+            return user
+        raise ValueError(f"User {email} already exists")
