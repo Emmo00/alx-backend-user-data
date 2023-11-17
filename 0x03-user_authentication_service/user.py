6d810b96d698a0c3f8f5694129fa4f26057b042c
Emmanuel Nwafor <nwaforemmanuel005@gmail.com>
2023-11-17 02:04:20 +0100

    db

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -4,12 +4,14 @@
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
+Base = declarative_base()
 
-class User(declarative_base()):
+
+class User(Base):
     """User model
     """
     __tablename__ = 'users'
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     email = Column(String, nullable=False)
     hashed_password = Column(String, nullable=False)
     session_id = Column(String, nullable=True)
