d0405c40b231a1b7ab46c0b083cf581ece2dacb8
Emmanuel Nwafor <nwaforemmanuel005@gmail.com>
2023-11-17 05:24:02 +0100

    annotate

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -40,7 +40,7 @@ class DB:
         self._session.commit()
         return user
 
-    def find_user_by(self, **kwargs):
+    def find_user_by(self, **kwargs) -> User:
         """find users from keywords
         """
         for filter in kwargs:
@@ -51,7 +51,7 @@ class DB:
             raise NoResultFound
         return user
 
-    def update_user(self, user_id: int, **kwargs):
+    def update_user(self, user_id: int, **kwargs) -> None:
         """update user
         """
         try:
