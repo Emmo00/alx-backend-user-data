0990023d16263a293e3ef1f790f71b65aa7944ee
Emmanuel Nwafor <nwaforemmanuel005@gmail.com>
2023-11-17 05:25:41 +0100

    none

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -35,6 +35,8 @@ class DB:
     def add_user(self, email: str, hashed_password: str) -> User:
         """creates a user
         """
+        if not email or not hashed_password:
+            return None
         user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
         self._session.commit()
